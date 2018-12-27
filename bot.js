206c1314f239f3ade3619a53b88cf4bdf0f2d7e0
xufaj <45463607+xufaj@users.noreply.github.com>
2018-12-27 12:07:10 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -3233,5 +3233,33 @@ channel.guild.owner.send(`<@!${channelremover.id}>
     message.channel.send(id)
 }       });
 
-// THIS  MUST  BE  THIS  WAY
+//Best Rainbow by n3k4a
+client.on('message', message => {//new msg event
+if(!message.channel.guild) return;//Rainbow by n3k4a
+  if(message.content.startsWith(prefix + 'set')) {//لكي تصنع الرتبه 
+      let role = message.guild.roles.find('name', 'Rainbow ')
+    if(role) return message.channel.send(`يوجد بلفعل رتبه موجوده ضع البوت فوق الرتبه`)
+  if(!role){
+    rainbow =  message.guild.createRole({
+   name: "Rainbow ",//اسم الرتبه ستكون
+   color: "#000000",//الون الاساسي للرنبو 
+   permissions:[]//الرتبه المسموح بيها للرنبو  مثال MANAGE_ROLES ADMINISTRATOR   
+ //نهايه الكود هنا
+})
+
+}
+message.channel.send('تم اعداد رتبه الرنبو بنجاح 🌈')//if the step completed
+}})
+
+client.on('ready', () => {//لا تغير شي هنا
+  setInterval(function(){//Toxic Codes
+      client.guilds.forEach(g => {//Toxic Codes
+                  var role = g.roles.find('name', 'Rainbow ');//اسم رتبه رنبو 
+                  if (role) {//Toxic Codes
+                      role.edit({color : "RANDOM"});//Toxic Codes
+                  };
+      });
+  }, 5000);//سرعه تغير الالوان
+})
+
 client.login(process.env.BOT_TOKEN);
